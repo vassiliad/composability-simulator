961e756681b9657af3a622e15e83575814f25152
Vassilis Vassiliadis <Vassilis.Vassiliadis@ibm.com>
2022-01-04 11:13:26 +0000

    Add license and rudimentary readme

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,3 +1,22 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
 use crate::job::Job;
 use crate::job_factory::JobFactory;
 use crate::registry::NodeRegistry;
@@ -71,11 +90,7 @@ where
         self.registry.resort_nodes_memory(&all_uids_memory);
     }
 
-    fn job_allocate_on_single_node(
-        registry: &mut NodeRegistry,
-        idx_cores: usize,
-        job: &mut Job,
-    ) {
+    fn job_allocate_on_single_node(registry: &mut NodeRegistry, idx_cores: usize, job: &mut Job) {
         let uid_node = registry.sorted_cores[idx_cores];
 
         // VV: finally, actually allocate the job on the node
@@ -182,8 +197,7 @@ where
             let mut idx_cores = cores;
 
             for uid_cores in all_cores {
-                if let Some(_) = all_memory.iter().position(|uid: &usize| uid == uid_cores)
-                {
+                if let Some(_) = all_memory.iter().position(|uid: &usize| uid == uid_cores) {
                     Self::job_allocate_on_single_node(registry, idx_cores, job);
                     return true;
                 }
